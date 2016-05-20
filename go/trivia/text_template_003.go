62ffc441c5f434485e05898fa85a8e88b6a8d3e9
Haipeng <njucs_wuhaipeng@163.com>
2016-05-20 13:36:45 +0800

    another text/tmplate example

---
+++ go/trivia/text_template_003.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "bytes"
+    "fmt"
+    "text/template"
+)
+
+type X struct {
+    A []*Y
+}
+
+func (x *X) Funx(a int) []*Y {
+    x.A = make([]*Y, a)
+    for i := 0; i < a; i++ {
+        x.A[i] = new(Y)
+        x.A[i].B = i
+    }
+
+    return x.A
+}
+
+type Y struct {
+    B int
+}
+
+func main() {
+    yml := `a{{range $x := .Funx 3}}x{{$x.B}}y{{end}}b`
+    x := new(X)
+
+    var b bytes.Buffer
+    t, _ := template.New("").Parse(yml)
+    t.Execute(&b, x)
+    fmt.Println(b.String())
+}
