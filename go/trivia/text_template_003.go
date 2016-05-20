9d179362f0777cff3daa25a80184463327cefe34
Haipeng <njucs_wuhaipeng@163.com>
2016-05-20 13:46:20 +0800

    text/template example update

--- go/trivia/text_template_003.go
+++ go/trivia/text_template_003.go
@@ -24,8 +24,12 @@ type Y struct {
     B int
 }
 
+func (y *Y) Funy(a int) int {
+    return y.B + a
+}
+
 func main() {
-    yml := `a{{range $x := .Funx 3}}x{{$x.B}}y{{end}}b`
+    yml := `a{{range $x := .Funx 3}}x{{$x.Funy 10}}y{{end}}b`
     x := new(X)
 
     var b bytes.Buffer
